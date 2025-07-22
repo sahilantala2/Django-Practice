906f53ded04702b90c934a5bb872a415d8fb88be
akashpie <akashpie3@gmail.com>
2025-07-22 18:11:58 +0530

    html pages

--- vege/models.py
+++ vege/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import SET_NULL
 
 
 # Create your models here.
 class Receipe(models.Model):
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
     recepie_name = models.CharField(max_length=100)
     recepie_description = models.TextField()
     recepie_image = models.ImageField(upload_to="recepie_img")
--- vege/views.py
+++ vege/views.py
@@ -1,3 +1,6 @@
+from http.client import HTTPResponse
+
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.template.context_processors import request
 from .models import *
@@ -19,6 +22,61 @@ def receipes(request):
         return  redirect('/receipes/')
 
     recipies = Receipe.objects.all()
-    context = {'recip' : recipies}
 
+    if request.GET.get('search'):
+        recipies = recipies.filter(recepie_name__icontains = request.GET.get('search'))
+
+    context = {'recip' : recipies}
     return render(request,'receipe.html',context)
+
+
+def delete_rece(request,id):
+
+    queryset = Receipe.objects.get(id = id)
+    queryset.delete()
+    return redirect('/receipes/')
+
+def update_rece(request,id):
+
+    queryset = Receipe.objects.get(id = id)
+
+    if request.method == "POST":
+        data = request.POST
+        recepie_name = data.get('recepie_name')
+        recepie_description = data.get('recepie_description')
+        recepie_image = request.FILES.get('recepie_image')
+
+        queryset.recepie_name = recepie_name
+        queryset.recepie_description = recepie_description
+
+        if recepie_image:
+            queryset.recepie_image = recepie_image
+
+        queryset.save()
+
+        return redirect('/receipes/')
+    context = {'rec':queryset}
+
+
+    return render(request,'update_reci.html',context)
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        user_name = request.POST.get('user_name')
+        password = request.POST.get('password')
+
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            user_name = user_name
+        )
+
+        user.set_password(password)
+        user.save()
+
+    return render(request,'register.html')
